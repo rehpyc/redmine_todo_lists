81bf1c31341bc5c121c66a6105e90d063e00b506
David.Ou <gault_1@icloud.com>
2020-06-24 10:07:35 +0900

    Added permission items.

--- init.rb
+++ init.rb
@@ -1,8 +1,16 @@
 Redmine::Plugin.register :redmine_todo_lists do
-  name 'Redmine Todo Lists plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name        'Redmine Todo Lists plugin'
+  author      'David.Ou'
+  description 'This plugin provides todo list management features.'
+  version     '0.0.1'
+  url         'https://github.com/rehpyc/redmine_todo_lists'
+  author_url  'https://github.com/rehpyc'
+
+  project_module :todo_lists do
+    permission :view_todos, { todos:  [:index, :show] }
+    permission :edit_todos, { todos:  [:new, :create, :edit, :update, :destory, :toggle_complete, :sort],
+                              issues: [:new, :create, :edit, :update, :destory, :toggle_complete, :sort] }
+    permission :personal_todos, { mytodos: [:index, :new, :create, :edit, :update, :destory, :toggle_complete, :sort] }
+  end
+
 end
